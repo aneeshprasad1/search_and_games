61c63bf56975071b68570c023b35da671e3d85ce
sohamshah <sohamshah252@gmail.com>
2015-09-24 18:27:54 -0700

    first comm

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,7 +18,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -61,7 +61,7 @@ class SearchProblem:
         """
         Given a state, returns available actions.
         Returns a list of actions
-        """        
+        """
         util.raiseNotDefined()
 
     def getResult(self, state, action):
@@ -72,7 +72,7 @@ class SearchProblem:
 
     def getCost(self, state, action):
         """
-        Given a state and an action, returns step cost, which is the incremental cost 
+        Given a state and an action, returns step cost, which is the incremental cost
         of moving to that successor.
         """
         util.raiseNotDefined()
@@ -85,6 +85,48 @@ class SearchProblem:
         The sequence must be composed of legal moves.
         """
         util.raiseNotDefined()
+
+class Node:
+
+    def __init__(self, problem, parent=None, action=None):
+        if parent:
+            self.state = problem.getResult(parent.getState(), action)
+        else:
+            self.state = problem.getStartState()
+        self.parent = parent
+        self.action = action
+
+    def __repr__(self):
+        return "<Node %s>" % (self.state,)
+
+    def getState(self):
+        return self.state
+
+    def getParent(self):
+        return self.parent
+
+    def getAction(self):
+        return self.action
+
+    def path(self, problem):
+        root = problem.getStartState()
+        node = self
+        actions = []
+
+        while node.getState() != root:
+            action = node.getAction()
+            actions.insert(0, action)
+            node = node.getParent()
+        return actions
+
+    def expand(self, problem):
+        """
+        A helper function to expand a given node get all the possible actions and their
+        results as a list
+        """
+        actions = problem.getActions(self.state)
+        children = [Node(problem, self, action) for action in actions]
+        return children
 
 
 def tinyMazeSearch(problem):
@@ -103,11 +145,14 @@ def breadthFirstSearch(problem):
 
     You are not required to implement this, but you may find it useful for Q5.
     """
-    "*** YOUR CODE HERE ***"
+    root = problem.getStartState()
     frontier = util.Queue()
-    
+    frontier.push(root)
+    while (!frontier.isEmpty()) {
 
-    util.raiseNotDefined()
+    }
+    for
+#    util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
     """
@@ -116,6 +161,23 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+def depthLimitedDFS(depth, problem):
+    """
+    Depth limited DFS. Ran multiple times for different depths.
+    """
+    frontier = util.Stack()
+    while (depth > 0):
+        root = problem.getStartState() #starting node in graph
+        actions = problem.getActions(root) #edges from start state
+        resulting_states = []
+        for a in actions:
+            resulting_states.append(problem.getResult(root, a))
+
+
+        depth--
+
+
+
 def iterativeDeepeningSearch(problem):
     """
     Perform DFS with increasingly larger depth.
@@ -123,6 +185,12 @@ def iterativeDeepeningSearch(problem):
     Begin with a depth of 1 and increment depth by 1 at every step.
     """
     "*** YOUR CODE HERE ***"
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+
+
+
     util.raiseNotDefined()
 
 def aStarSearch(problem, heuristic=nullHeuristic):
